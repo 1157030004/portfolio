6145ba433e3db51ecca18e9dab3893e7526e72eb
1157030004 <1157030004@student.uinsgd.ac.id>
2021-07-06 15:46:34 +0700

    running on works

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Menu from "./components/menu/Menu";
 function App() {
 	const [menuOpen, setMenuOpen] = useState(false)
 	return (
-		<div className="App">
+		<div className="app">
 			<Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
 			<Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
 			<div className="sections">
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,9 +1,29 @@
-import "./works.sass"
+import "./works.scss"
 
 function Works() {
     return (
         <div className="works" id="works">
-            
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src="assets/mobile.png" alt="" />
+                                </div>
+                                <h2>Title</h2>
+                                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
+                                <span>Projects</span>
+                            </div>
+                        </div>
+                        <div className="right">
+                            <img src="https://media.suara.com/pictures/653x366/2020/08/24/51982-halte-bus-totoro-dok-instagramsatomisakas.jpg" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <img src="assets/arrow.png" className="arrow left" alt="" />
+            <img src="assets/arrow.png" className="arrow right" alt="" />
         </div>
     )
 }
